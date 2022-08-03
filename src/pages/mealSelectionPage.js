0a17ca8243f72538b39e333dff99267b241784a6
Cosimoyannis <Cosimo.t@live.de>
2022-08-03 14:06:17 +0200

    view fix

--- src/pages/mealSelectionPage.js
+++ src/pages/mealSelectionPage.js
@@ -57,11 +57,11 @@ function Home() {
           <div class="card h-100 shadow flex-row" style={{backgroundColor: "#FFC529"}}  >
 
         
-          <img class="col align-self-center" src={ad2} style={{marginLeft: "10vw"}} alt="" width="130" height=""></img>
+          <img class="col align-self-center" src={ad2} style={{marginLeft: "4vw"}} alt="" width="100" height=""></img>
 
-            <div class="card-body col align-self-center" style={{marginLeft: "10vw"}}>
-              <h5 class="card-title " style={{color: "#C65A39"}}><b>Today is Pizza-Day!</b></h5>
-              <h5 class=" fw-light fs-6"> Enter the code PIZZA at checkout and save 10% </h5>
+            <div class="card-body col align-self-center" style={{marginLeft: ""}}>
+              <h5 class="card-title " style={{color: "#C65A39"}}><b>Today is <br />Pizza-Day!</b></h5>
+              <h5 class=" fw-light fs-6"> Enter the code PIZZA at checkout and <br />save 10% </h5>
               
             </div>
         
@@ -77,9 +77,9 @@ function Home() {
           <div class="card h-100 shadow flex-row text-start" >
 
         
-          <img class="col align-self-center" src={burger} style={{marginLeft: "10vw"}} alt="" width="100" height="100"></img>
+          <img class="col align-self-center" src={burger} style={{marginLeft: "5vw"}} alt="" width="100" height=""></img>
 
-            <div class="card-body col align-self-center" style={{marginLeft: "10vw"}}>
+            <div class="card-body col align-self-center" style={{marginLeft: "5vw"}}>
               <h5 class="card-title"><b>{users[1].name}</b></h5>
               <h5 class="text-muted fw-light fs-6"> 16 left | <svg style={{marginBottom: "1.1vw"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
               <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
@@ -107,10 +107,10 @@ function Home() {
           <div class="card h-100 shadow flex-row" >
 
         
-          <img class="col align-self-center" src={pizza} style={{marginLeft: "10vw"}} alt="" width="100" height="100"></img>
+          <img class="col align-self-center" src={pizza} style={{marginLeft: "5vw"}} alt="" width="100" height=""></img>
 
-            <div class="card-body col align-self-center" style={{marginLeft: "10vw"}}>
-              <h5 class="card-title"><b>{users[0].name}</b></h5>
+            <div class="card-body col align-self-center" style={{marginLeft: "5vw"}}>
+              <h5 class="card-title"><b>{users[1].name}</b></h5>
               <h5 class="text-muted fw-light fs-6"> 16 left | <svg style={{marginBottom: "1.1vw"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
               <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
               </svg> 4.5 </h5>
@@ -136,9 +136,9 @@ function Home() {
           <div class="card h-100 shadow flex-row text-start" >
 
         
-          <img class="col align-self-center" src={burger} style={{marginLeft: "10vw"}} alt="" width="100" height="100"></img>
+          <img class="col align-self-center" src={burger} style={{marginLeft: "5vw"}} alt="" width="100" height=""></img>
 
-            <div class="card-body col align-self-center" style={{marginLeft: "10vw"}}>
+            <div class="card-body col align-self-center" style={{marginLeft: "5vw"}}>
               <h5 class="card-title"><b>{users[1].name}</b></h5>
               <h5 class="text-muted fw-light fs-6"> 16 left | <svg style={{marginBottom: "1.1vw"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
               <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
@@ -164,9 +164,9 @@ function Home() {
           <div class="card h-100 shadow flex-row" >
 
         
-          <img class="col align-self-center" src={pizza} style={{marginLeft: "10vw"}} alt="" width="100" height="100"></img>
+          <img class="col align-self-center" src={pizza} style={{marginLeft: "5vw"}} alt="" width="100" height=""></img>
 
-            <div class="card-body col align-self-center" style={{marginLeft: "10vw"}}>
+            <div class="card-body col align-self-center" style={{marginLeft: "5vw"}}>
               <h5 class="card-title"><b>{users[0].name}</b></h5>
               <h5 class="text-muted fw-light fs-6"> 16 left | <svg style={{marginBottom: "1.1vw"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
               <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
@@ -193,9 +193,9 @@ function Home() {
           <div class="card h-100 shadow flex-row text-start" >
 
         
-          <img class="col align-self-center" src={burger} style={{marginLeft: "10vw"}} alt="" width="100" height="100"></img>
+          <img class="col align-self-center" src={burger} style={{marginLeft: "5vw"}} alt="" width="100" height=""></img>
 
-            <div class="card-body col align-self-center" style={{marginLeft: "10vw"}}>
+            <div class="card-body col align-self-center" style={{marginLeft: "5vw"}}>
               <h5 class="card-title"><b>{users[1].name}</b></h5>
               <h5 class="text-muted fw-light fs-6"> 16 left | <svg style={{marginBottom: "1.1vw"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
               <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
