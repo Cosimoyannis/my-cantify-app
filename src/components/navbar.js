7de336c4ccc115561a268e18274a56a61b4efee7
Cosimoyannis <Cosimo.t@live.de>
2022-08-29 14:52:55 +0200

    version fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,13 +4,13 @@ const Navbar = () => {
      <nav class="navbar bg-white fixed-bottom rounded-pill py-4 shadow">
               
         <div class="container d-flex justify-content-evenly">
-          <a class="navbar-brand" href="/">
+          <a class="navbar-brand" href="/mealSelectionPage">
             <img src="https://cdn.glitch.global/0c15c725-e764-4776-a32b-28fdb0e82a47/home_orange.png?v=1656865821277" alt="" width="30" height="30"></img>
           </a>
-           <a class="navbar-brand" href="/welcomepage">
+           <a class="navbar-brand" href="/mealSelectionPage">
             <img src="https://cdn.glitch.global/0c15c725-e764-4776-a32b-28fdb0e82a47/heart_orange.png?v=1656865820935" alt="" width="24" height="24"></img>
           </a>
-           <a class="navbar-brand" href="/welcomepage">
+           <a class="navbar-brand" href="/mealSelectionPage">
             <img src="https://cdn.glitch.global/0c15c725-e764-4776-a32b-28fdb0e82a47/search_orange.png?v=1656865821143" alt="" width="30" height="30"></img>
           </a>
           <a class="navbar-brand" href="/shoppingcartPage">
