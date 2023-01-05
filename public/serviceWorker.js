b895c819632fe4437f2eaea9db7116897bcfdc11
Cosimoyannis <Cosimo.t@live.de>
2023-01-05 18:45:01 +0100

    offline page edit

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,12 +1,14 @@
 // service worker for basic offline view
 
 const CACHE = "cantify-cache";
-const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = 'errorpic.png';
+const offlineFallbackPagetwo = 'offline.html'; 
+
+var URLSTOCACHE = [offlineFallbackPage];
 
 self.addEventListener("install", async event  => {
-  // store offline.HTML  in cache 
-  
-event.waitUntil(caches.open(CACHE).then(cache => cache.add(offlineFallbackPage)));
+  // store files in cache 
+event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(URLSTOCACHE)));
 });
 
 
@@ -32,4 +34,16 @@ self.addEventListener("fetch", event => {
       }) ()
     );
   }
+}); 
+
+
+/*
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
+*/
